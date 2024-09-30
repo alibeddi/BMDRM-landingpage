d877a286e75960fb370f74da02cc6d44a5adac77
chafroudtarek <chafroudtarek6@gmail.com>
2024-09-30 13:21:12 +0100

    remove border that block video player

--- layouts/components/VideoPopupServer.js
+++ layouts/components/VideoPopupServer.js
@@ -2,8 +2,9 @@ import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import VideoFrame from "./VideoFrame";
 
 function VideoPopupServer({ id, thumbnail, width = 700, height = 394 }) {
+  //animate
   return (
-    <div className="animate relative  flex overflow-hidden rounded-2xl">
+    <div className=" relative  flex overflow-hidden">
       <div className="aspect-video w-full">
         {" "}
         <VideoFrame />{" "}
